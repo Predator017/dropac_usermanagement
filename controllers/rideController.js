7a5fb04949ee0a2789b3b29e63e4b33ea334d413
vignesh-codekshun <vignesh.k@codekshun.com>
2025-03-01 11:44:45 +0530

    updated existing req

--- controllers/rideController.js
+++ controllers/rideController.js
@@ -20,7 +20,7 @@ exports.createRideRequest = async (req, res) => {
   try {
     // Check for an existing pending ride request
     const existingRequest = await Ride.findOne({ userId, status: "pending" });
-
+    console.log(existingRequest);
     if (existingRequest) {
       if (new Date() > new Date(existingRequest.timeoutAt)) {
         existingRequest.status = "cancelled";
